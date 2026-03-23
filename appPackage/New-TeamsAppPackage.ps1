4044c13f24e205d42d0caa3171d665281a4778a4
Eric Anderson <eric@eanderson.de>
2026-03-23 19:26:43 +0100

    fix(#171): clear stale blobs on tab deploy, fix TeamsAppId default in publish script

--- appPackage/New-TeamsAppPackage.ps1
+++ appPackage/New-TeamsAppPackage.ps1
@@ -46,7 +46,7 @@ param(
     [switch]$Open,
     [switch]$SkipValidation,
     [switch]$Publish,
-    [string]$TeamsAppId = "e2883966-f38e-40e8-a1c0-d1145bdb23c5",
+    [string]$TeamsAppId = "13e1b315-79d3-4a0e-b234-1c1128883a05",
     [string]$TenantId = "51b1f02a-e19b-4089-a5f6-3ebb72835521",
     [switch]$RequiresReview
 )
